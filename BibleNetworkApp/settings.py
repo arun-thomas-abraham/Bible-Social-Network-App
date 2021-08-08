9d29170c2383086789cccbd351ea6a5295697f7e
Arun Abraham <arun.abraham14@gmail.com>
2021-08-07 21:56:48 -0500

    remove secret key

--- BibleNetworkApp/settings.py
+++ BibleNetworkApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ef#xo=flk-q*u6-n-a!ffx%ed5xfsfcx8kk%3%$xbgwv&@%##d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
